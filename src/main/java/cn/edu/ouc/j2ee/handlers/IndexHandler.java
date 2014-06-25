d834016dbc08f448651816fb9ee0b1558b6b2292
lsmtty <2465407890@qq.com>
2014-06-25 08:14:58 +0800

    修复了文章的可见性 文章的按时间排序功能

--- src/main/java/cn/edu/ouc/j2ee/handlers/IndexHandler.java
+++ src/main/java/cn/edu/ouc/j2ee/handlers/IndexHandler.java
@@ -10,15 +10,21 @@ import cn.edu.ouc.j2ee.entities.PostEntity;
 public class IndexHandler extends BaseHandler {
     @Override
     public Object handle() {
+        List<PostEntity> posts;
+        if(currentUser==null){
+            posts = (List<PostEntity>)database.createQuery(
+             "From PostEntity where publicity = true order by date desc").list();
+        }
+        else {
+            String UserId = Integer.toString(currentUser.getId());
+            posts = (List<PostEntity>) database.createQuery(
+                    "From PostEntity where publicity = true or author.id = ? order by date desc").setString(0,UserId).list();
 
-        List<PostEntity> posts = (List<PostEntity>)database.createQuery(
-                "From PostEntity order by date desc").list();
-
-        for (PostEntity post: posts) {
+        }
+        for (PostEntity post : posts) {
             post.initCommentCount();
             post.initIsMy(currentUser);
         }
-
         modal.put("posts", posts);
         return rendered();
 
