8be8f6e60e98260fdf9d9d86f362c4e1fd99e95f
D6C92FE5 <D6C92FE5@hotmail.com>
2014-06-25 11:44:36 +0800

    添加图标，少量修复

--- src/main/java/cn/edu/ouc/j2ee/handlers/IndexHandler.java
+++ src/main/java/cn/edu/ouc/j2ee/handlers/IndexHandler.java
@@ -10,21 +10,22 @@ import cn.edu.ouc.j2ee.entities.PostEntity;
 public class IndexHandler extends BaseHandler {
     @Override
     public Object handle() {
+
         List<PostEntity> posts;
-        if(currentUser==null){
+        if (currentUser==null){
             posts = (List<PostEntity>)database.createQuery(
-             "From PostEntity where publicity = true order by date desc").list();
+                    "From PostEntity where publicity = true order by date desc").list();
         }
         else {
-            String UserId = Integer.toString(currentUser.getId());
-            posts = (List<PostEntity>) database.createQuery(
-                    "From PostEntity where publicity = true or author.id = ? order by date desc").setString(0,UserId).list();
-
+            posts = (List<PostEntity>)database.createQuery(
+                    "From PostEntity where publicity = true or author.id = ? order by date desc")
+                    .setInteger(0, currentUser.getId()).list();
         }
         for (PostEntity post : posts) {
             post.initCommentCount();
             post.initIsMy(currentUser);
         }
+
         modal.put("posts", posts);
         return rendered();
 
